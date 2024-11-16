b5ce3a022e534ea0bceeebc68bf352268f826136
BassamAlim <Bassamalim@outlook.com>
2024-11-16 03:49:41 +0300

    migrating to material 3

--- app/src/main/java/bassamalim/hidaya/core/ui/components/SearchComponent.kt
+++ app/src/main/java/bassamalim/hidaya/core/ui/components/SearchComponent.kt
@@ -14,19 +14,11 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawWithContent
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.SolidColor
-import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.graphics.drawscope.clipRect
-import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import bassamalim.hidaya.R
@@ -39,35 +31,10 @@ fun SearchComponent(
     onSubmit: () -> Unit = {},
     onValueChange: (String) -> Unit = {}
 ) {
-    val lineColor = MaterialTheme.colorScheme.outline
-    val layoutDirection = LocalLayoutDirection.current
-
     TextField(
         value = value,
         onValueChange = onValueChange,
-        modifier = modifier
-            .padding(horizontal = 6.dp)
-            .drawWithContent {
-                drawContent()
-                clipRect {
-                    val strokeWidth = Stroke.DefaultMiter
-                    val y = size.height
-                    if (layoutDirection == LayoutDirection.Rtl) drawLine(
-                        brush = SolidColor(lineColor),
-                        strokeWidth = strokeWidth,
-                        cap = StrokeCap.Square,
-                        start = Offset.Zero.copy(x = 20F, y = y - 15),
-                        end = Offset(x = size.width - 120F, y = y - 15)
-                    )
-                    else drawLine(
-                        brush = SolidColor(lineColor),
-                        strokeWidth = strokeWidth,
-                        cap = StrokeCap.Square,
-                        start = Offset.Zero.copy(x = 140F, y = y - 15),
-                        end = Offset(x = size.width - 20F, y = y - 15)
-                    )
-                }
-            },
+        modifier = modifier.padding(horizontal = 6.dp),
         textStyle = TextStyle(color = Color.White, fontSize = 18.sp),
         leadingIcon = {
             Icon(
