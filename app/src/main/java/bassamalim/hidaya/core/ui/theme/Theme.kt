c47683e4581724aeed075b9d51d9f28ef8b72afb
BassamAlim <Bassamalim@outlook.com>
2024-11-19 23:11:18 +0300

    bug fixes and improvements

--- app/src/main/java/bassamalim/hidaya/core/ui/theme/Theme.kt
+++ app/src/main/java/bassamalim/hidaya/core/ui/theme/Theme.kt
@@ -1,8 +1,12 @@
 package bassamalim.hidaya.core.ui.theme
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.unit.LayoutDirection
 import bassamalim.hidaya.core.enums.Theme
@@ -16,10 +20,15 @@ fun AppTheme(
     CompositionLocalProvider(
         LocalLayoutDirection provides direction
     ) {
-        MaterialTheme(
-            colorScheme = getColors(theme),
-            content = content
-        )
+        MaterialTheme(colorScheme = getColors(theme)) {
+            Box(
+                Modifier
+                    .fillMaxSize()
+                    .background(MaterialTheme.colorScheme.background)
+            ) {
+                content()
+            }
+        }
     }
 }
 
