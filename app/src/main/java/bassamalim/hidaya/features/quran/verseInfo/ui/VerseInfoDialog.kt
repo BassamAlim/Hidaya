58a722bdb993df4184efb1389f2fcb56ff429888
BassamAlim <Bassamalim@outlook.com>
2025-03-17 00:36:10 +0300

    bug fixes and improvements

--- app/src/main/java/bassamalim/hidaya/features/quran/verseInfo/ui/VerseInfoDialog.kt
+++ app/src/main/java/bassamalim/hidaya/features/quran/verseInfo/ui/VerseInfoDialog.kt
@@ -7,7 +7,9 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.BookmarkAdd
 import androidx.compose.material.icons.filled.BookmarkRemove
@@ -46,7 +48,9 @@ fun VerseInfoDialog(viewModel: VerseInfoViewModel) {
 @Composable
 private fun SheetContent(viewModel: VerseInfoViewModel, state: VerseInfoUiState) {
     Column(
-        modifier = Modifier.padding(16.dp),
+        modifier = Modifier
+            .verticalScroll(rememberScrollState())
+            .padding(16.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
         BookmarkOptionsSection(
